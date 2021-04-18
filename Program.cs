cfc8f2c90a19c33f8a5a1ed26ff45c73adef56c7
Mark Dewey <mark.t.dewey@gmail.com>
2021-04-17 20:24:49 -0400

    deployed

--- Program.cs
+++ Program.cs
@@ -3,24 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using StudentLifeTracker.Models;
 
 namespace BottleCapApi
 {
-    public class Program
+  public class Program
+  {
+    public static async Task Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+      var host = CreateHostBuilder(args).Build();
+      using (var scope = host.Services.CreateScope())
+      {
+        var context = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+        Console.WriteLine("Starting to migrate database....");
+        await context.Database.MigrateAsync();
+        Console.WriteLine("Database is up to date, #party time");
+      }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+      await host.RunAsync();
     }
+
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
 }
