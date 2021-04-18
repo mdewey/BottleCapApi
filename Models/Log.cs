aeace98636e48bf3a41ff5eed666364929b86de1
Mark Dewey <mark.t.dewey@gmail.com>
2021-04-18 13:15:58 -0400

    welp

--- Models/Log.cs
+++ Models/Log.cs
@@ -10,7 +10,7 @@ namespace BottleCapApi.Models
 
     public string Response { get; set; }
 
-    public DateTime When { get; set; } = DateTime.UtcNow.AddDays();
+    public DateTime When { get; set; } = DateTime.UtcNow;
 
 
   }
