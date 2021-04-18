329d2d1ce561a43d375fa596091a28000650a38b
Mark Dewey <mark.t.dewey@gmail.com>
2021-04-18 13:15:46 -0400

    fixed log time

--- Models/Log.cs
+++ Models/Log.cs
@@ -10,7 +10,7 @@ namespace BottleCapApi.Models
 
     public string Response { get; set; }
 
-    public DateTime When { get; set; } = DateTime.UtcNow.AddDays(-20);
+    public DateTime When { get; set; } = DateTime.UtcNow.AddDays();
 
 
   }
